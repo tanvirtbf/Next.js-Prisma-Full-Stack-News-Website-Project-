471b10c0e5bd47671881eb0b78d0b7af7b7fa5d6
tanvirtbf <tanver018765@gmail.com>
2024-04-28 16:15:37 +0600

    user create api complete and test it

---
+++ Project Folder/main-app/src/app/api/user/registration/route.js
@@ -0,0 +1,22 @@
+import { PrismaClient } from "@prisma/client";
+import { NextResponse } from "next/server";
+
+export async function POST(req,res){
+  try {
+    let reqBody = await req.json();
+    reqBody.otp = "0";
+    const prisma = new PrismaClient();
+    const result = await prisma.users.create({
+      data: reqBody,
+    })
+    return NextResponse.json({
+      status: 'success',
+      data: result,
+    })
+  } catch (e) {
+    return NextResponse.json({
+      status: 'fail',
+      data: e,
+    })
+  }
+}
