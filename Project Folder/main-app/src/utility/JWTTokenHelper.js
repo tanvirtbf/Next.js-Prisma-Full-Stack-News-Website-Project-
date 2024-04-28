db3bbedd6f1077a4fe3b4c8a5b864088dff7e07a
tanvirtbf <tanver018765@gmail.com>
2024-04-28 16:41:19 +0600

    complete jwt functionality

--- Project Folder/main-app/src/utility/JWTTokenHelper.js
+++ Project Folder/main-app/src/utility/JWTTokenHelper.js
@@ -1,19 +1,19 @@
-import {jwtVerify,SignJWT} from 'jose'
+import { jwtVerify, SignJWT } from "jose";
 
-export async function CreateToken(email,id){
-  const secret=new TextEncoder().encode(process.env.JWT_SECRET);
-  const payload={email:email, id:id}
+export async function CreateToken(email, id) {
+  const secret = new TextEncoder().encode(process.env.JWT_SECRET);
+  const payload = { email: email, id: id };
   let token = await new SignJWT(payload)
-    .setProtectedHeader({alg: 'HS256'}) // jwt HS256 algorithm use kore encode kora hocche
-    .setIssuedAt() //jwt kokhon set kora hocche 
+    .setProtectedHeader({ alg: "HS256" }) // jwt HS256 algorithm use kore encode kora hocche
+    .setIssuedAt() //jwt kokhon set kora hocche
     .setIssuer(process.env.JWT_ISSUER) // kon url e set kora hocche
     .setExpirationTime(process.env.JWT_EXPIRATION_TIME) //koto somoy porjonto amader jwt enable thakbe
-    .sign(secret)
-  return token
+    .sign(secret);
+  return token;
 }
 
-export async function VerifyToken(token){
-  const secret=new TextEncoder().encode(process.env.JWT_SECRET);
-  const decoded = await jwtVerify(token,secret)
-  return decoded['payload']
+export async function VerifyToken(token) {
+  const secret = new TextEncoder().encode(process.env.JWT_SECRET);
+  const decoded = await jwtVerify(token, secret);
+  return decoded["payload"];
 }
