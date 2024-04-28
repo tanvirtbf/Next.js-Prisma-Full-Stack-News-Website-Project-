4383814c74d528de281af1e5b5040a4a55bb4d6a
tanvirtbf <tanver018765@gmail.com>
2024-04-28 16:35:02 +0600

    CreateToken function created which function create jwt token with information

---
+++ Project Folder/main-app/src/utility/JWTTokenHelper.js
@@ -0,0 +1,13 @@
+import {jwtVerify,SignJWT} from 'jose'
+
+export async function CreateToken(email,id){
+  const secret=new TextEncoder().encode(process.env.JWT_SECRET);
+  const payload={email:email, id:id}
+  let token = await new SignJWT(payload)
+    .setProtectedHeader({alg: 'HS256'}) // jwt HS256 algorithm use kore encode kora hocche
+    .setIssuedAt() //jwt kokhon set kora hocche 
+    .setIssuer(process.env.JWT_ISSUER) // kon url e set kora hocche
+    .setExpirationTime(process.env.JWT_EXPIRATION_TIME) //koto somoy porjonto amader jwt enable thakbe
+    .sign(secret)
+  return token
+}
