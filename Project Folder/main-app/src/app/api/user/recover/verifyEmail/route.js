b45a490be012f77abbaa552b91547258a31cca18
tanvirtbf <tanver018765@gmail.com>
2024-04-29 12:01:04 +0600

    create verifyOTP api and test it

--- Project Folder/main-app/src/app/api/user/recover/verifyEmail/route.js
+++ Project Folder/main-app/src/app/api/user/recover/verifyEmail/route.js
@@ -2,40 +2,40 @@ import { SendEmail } from "@/utility/EmailUtility";
 import { PrismaClient } from "@prisma/client";
 import { NextResponse } from "next/server";
 
-export async function GET(req,res){
+export async function GET(req, res) {
   try {
     const prisma = new PrismaClient();
-    let {searchParams} = new URL(req.url)
-    let email = searchParams.get('email')
+    let { searchParams } = new URL(req.url);
+    let email = searchParams.get("email");
 
     //User Count
     const count = await prisma.users.count({
-      where:{email:email}
-    })
-    
-    if(count === 1){
-      let code = Math.floor(100000+Math.random()*900000);
-      let EmailText = `Your OTP Code is ${code}`
-      let EmailSubject = "Next News Verification Code"
-      await SendEmail(email,EmailText,EmailSubject)
+      where: { email: email },
+    });
+
+    if (count === 1) {
+      let code = Math.floor(100000 + Math.random() * 900000);
+      let EmailText = `Your OTP Code is ${code}`;
+      let EmailSubject = "Next News Verification Code";
+      await SendEmail(email, EmailText, EmailSubject);
       let result = await prisma.users.update({
-        where:{email:email},
-        data:{otp:code.toString()}
-      })
+        where: { email: email },
+        data: { otp: code.toString() },
+      });
       return NextResponse.json({
-        status:'Success',
+        status: "Success",
         data: result,
-      })
-    }else{
+      });
+    } else {
       return NextResponse.json({
-        status: 'Fail',
-        data:'No User Found',
-      })
+        status: "Fail",
+        data: "No User Found",
+      });
     }
   } catch (error) {
     return NextResponse.json({
-      status: 'fail',
+      status: "fail",
       data: error,
-    })
+    });
   }
 }
---
+++ Project Folder/main-app/src/app/api/user/recover/verifyOTP/route.js
@@ -0,0 +1,30 @@
+import { PrismaClient } from "@prisma/client";
+import { NextResponse } from "next/server";
+
+export async function POST(req,res){
+  try {
+    const prisma = new PrismaClient();
+    let reqBody = await req.json();
+
+    let count = await prisma.users.count({
+      where:reqBody,
+    })
+    if(count === 1){
+      return NextResponse.json({
+        status:'success',
+        data: count,
+      })
+    }else{
+      return NextResponse.json({
+        status:'fail',
+        data: 'No User Found'
+      })
+    }
+
+  } catch (error) {
+    return NextResponse.json({
+      staus:'fail',
+      data: error,
+    })
+  }
+}
